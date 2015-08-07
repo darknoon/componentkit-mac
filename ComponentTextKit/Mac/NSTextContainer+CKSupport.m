34a29c33ab7a4a820f5d09eec265879a08d1fc2a
Andrew Pouliot <andrewpouliot@fb.com>
2015-08-06 17:14:25 -0700

    Fix crash / spin on 10.11

--- ComponentTextKit/Mac/NSTextContainer+CKSupport.m
+++ ComponentTextKit/Mac/NSTextContainer+CKSupport.m
@@ -1,11 +1,8 @@
 
 #import "NSTextContainer+CKSupport.h"
 
-@implementation NSTextContainer (CKPlatform)
-
-- (CGSize)size
-{
-  return self.containerSize;
-}
-
-@end
+//@implementation NSTextContainer (CKPlatform)
+//
+// Previously, we implemented this to return containerSize, but that's actually unnecessary since -size is already supported on 10.10 and returns the same thing (verified via disasm)
+//
+//@end
